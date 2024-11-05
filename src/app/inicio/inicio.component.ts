0fa237b7b18a683bf8cecdf932fb35eb845cef00
SamyAlinne <117871370+SamyAlinne@users.noreply.github.com>
2024-11-05 09:38:13 -0600

    Botón de música

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,32 +1,60 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'; // Importa Router
+import { Router } from '@angular/router'; 
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
   showRules: boolean = false;
+  record: number = 0;
+  musicPlayer: HTMLAudioElement | null = null;
+  musicIcon: string = '/assets/music-off.png'; // Ícono inicial
 
-  record: number = 0; // Inicializa el récord
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) {} // Inyecta Router
-
-  startGame() {
-    this.router.navigate(['/game']); // Redirige al componente de juego
+  ngOnInit(): void {
+    // Inicializa el reproductor de música al cargar el componente
+    this.initializeMusicPlayer();
   }
 
-  // Método para mostrar/ocultar las reglas
-  toggleRules() {
+  initializeMusicPlayer(): void {
+    if (!this.musicPlayer) {
+      this.musicPlayer = new Audio('/assets/musicadefondo.mp3');
+      this.musicPlayer.loop = true;
+      this.musicPlayer.volume = 0.5;
+      this.musicPlayer.play();
+      this.musicIcon = '/assets/music.png';
+    }
+  }
+
+  toggleMusic(): void {
+    if (this.musicPlayer) {
+      if (this.musicPlayer.paused) {
+        this.musicPlayer.play();
+        this.musicIcon = '/assets/music.png';
+      } else {
+        this.musicPlayer.pause();
+        this.musicIcon = '/assets/music-off.png';
+      }
+    }
+  }
+
+  startGame(): void {
+    this.router.navigate(['/game']);
+  }
+
+  toggleRules(): void {
     this.showRules = !this.showRules;
   }
-  toggleFullScreen() {
+
+  toggleFullScreen(): void {
     if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen(); // Pide pantalla completa
+      document.documentElement.requestFullscreen();
     } else {
       if (document.exitFullscreen) {
-        document.exitFullscreen(); // Salir de pantalla completa
+        document.exitFullscreen();
       }
     }
   }
