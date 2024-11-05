f40460c11beb12ea4e43c064fc375ba091097c07
Mario <mariodominguez1809@gmail.com>
2024-11-04 20:56:16 -0600

    Ya esta la animación de la capsula con sus respectivos botones

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -11,6 +11,10 @@ export class GameComponent {
   isSendButtonPressed: boolean = false;
   isBackButtonPressed: boolean = false;
   isRegresarButtonPressed: boolean = false;
+  leftCapsuleState: string = 'assets/abierto1.png';
+  rightCapsuleState: string = 'assets/cerrado2.png';
+  isLeftCapsuleOpen: boolean = true;
+  isRightCapsuleOpen: boolean = false;
 
   constructor(private router: Router) {}
 
@@ -30,7 +34,12 @@ export class GameComponent {
 
   onSendButtonMouseUp() {
     this.isSendButtonPressed = false;
-    this.send(); // Llama a la función de enviar al soltar el botón
+    if (this.isLeftCapsuleOpen) { // Solo permitir si la cápsula izquierda está abierta
+      this.closeLeftCapsule();
+      setTimeout(() => {
+        this.openRightCapsule();
+      }, 2000);
+    }
   }
 
   onBackButtonMouseDown() {
@@ -39,7 +48,12 @@ export class GameComponent {
 
   onBackButtonMouseUp() {
     this.isBackButtonPressed = false;
-    
+    if (this.isRightCapsuleOpen) { // Solo permitir si la cápsula derecha está abierta
+      this.closeRightCapsule();
+      setTimeout(() => {
+        this.openLeftCapsule();
+      }, 2000);
+    }
   }
 
   onRegresarButtonMouseDown() {
@@ -49,5 +63,38 @@ export class GameComponent {
   onRegresarButtonMouseUp() {
     this.isRegresarButtonPressed = false;
     this.goBack(); // Llama a la función de volver al soltar el botón
+  }
+
+  // Cambios de estado de las cápsulas
+  closeLeftCapsule() {
+    this.leftCapsuleState = 'assets/entreabierto1.png';
+    setTimeout(() => {
+      this.leftCapsuleState = 'assets/cerrado1.png';
+      this.isLeftCapsuleOpen = false;
+    }, 1000); // Cambiar a cerrada después de 1 segundo
+  }
+
+  openLeftCapsule() {
+    this.leftCapsuleState = 'assets/entreabierto1.png';
+    setTimeout(() => {
+      this.leftCapsuleState = 'assets/abierto1.png';
+      this.isLeftCapsuleOpen = true;
+    }, 1000);
+  }
+
+  closeRightCapsule() {
+    this.rightCapsuleState = 'assets/entreabierto2.png';
+    setTimeout(() => {
+      this.rightCapsuleState = 'assets/cerrado2.png';
+      this.isRightCapsuleOpen = false;
+    }, 1000); // Cambiar a cerrada después de 1 segundo
+  }
+
+  openRightCapsule() {
+    this.rightCapsuleState = 'assets/entreabierto2.png';
+    setTimeout(() => {
+      this.rightCapsuleState = 'assets/abierto2.png';
+      this.isRightCapsuleOpen = true;
+    }, 1000);
   }
 }
